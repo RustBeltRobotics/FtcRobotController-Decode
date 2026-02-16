2f1c18c1f7b7f7edd3b2e054da342d0f16d17609
SJ7F <235936019+SJ7F@users.noreply.github.com>
2026-02-16 23:45:31 +0100

    use new binary format for web telemetry streamer (untested)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WebTelemetryStreamer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WebTelemetryStreamer.java
@@ -4,10 +4,12 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class WebTelemetryStreamer implements Runnable {
 
@@ -17,11 +19,14 @@ public class WebTelemetryStreamer implements Runnable {
 
     DataOutputStream currentClientOut;
 
-    ConcurrentLinkedQueue<String> messageQueue;
+    ConcurrentLinkedQueue<byte[]> messageQueue;
 
     int port = 8082;
 
-
+    // Channel tracking
+    private final ConcurrentHashMap<String, Integer> channelMap = new ConcurrentHashMap<>();
+    private final AtomicInteger nextChannelId = new AtomicInteger(0);
+    private volatile boolean channelMapSent = false;
 
     @Override
     public void run() {
@@ -46,17 +51,19 @@ public class WebTelemetryStreamer implements Runnable {
         String response_start =
                 "HTTP/1.1 200 OK\r\n"+
                         "Date: Tue, 23 Apr 2024 10:30:00 GMT\r\n"+
-                        "Content-Type: text/event-stream\r\n"+
+                        "Content-Type: application/octet-stream\r\n"+
                         "Cache-Control: no-cache\r\n"+
                         "Connection: keep-alive\r\n"+
                         "Access-Control-Allow-Origin: *\r\n"+
-                        "\r\n0\r\n";
-
+                        "\r\n";
 
         this.currentClientOut = new DataOutputStream(currentClient.getOutputStream());
         currentClientOut.write(response_start.getBytes(StandardCharsets.UTF_8));
         currentClientOut.flush();
 
+        // Send channel map
+        sendChannelMap();
+        channelMapSent = true;
 
         System.out.println("Web telemetry streamer: Client connected");
     }
@@ -74,20 +81,16 @@ public class WebTelemetryStreamer implements Runnable {
 
             while (currentClient.isConnected()) {
                 try {
-                    String message = messageQueue.poll();
-//                    System.out.println("[wts] mq poll");
+                    byte[] message = messageQueue.poll();
                     if (message != null) {
                         try {
-                            this.currentClientOut.write(message.getBytes(StandardCharsets.UTF_8));
+                            this.currentClientOut.write(message);
                             // this.currentClientOut.flush();
                         } catch (IOException e) {
-                            // nothing
                             System.out.print("[wts] IOException: ");
                             System.out.println(e.getMessage());
                             break;
                         }
-                    } else {
-//                        System.out.println("[wts] Message null");
                     }
                     Thread.sleep(3); // bad, use blocking queue instead
                 } catch (InterruptedException e) {
@@ -96,13 +99,53 @@ public class WebTelemetryStreamer implements Runnable {
             }
 
             System.out.println("Web telemetry streamer: Client disconnected !");
+            channelMapSent = false;
         }
     }
 
+    private void sendChannelMap() throws IOException {
+        // Format: [marker byte 0xFF][num channels uint16][channel 0: uint16 name length][name string]...[channel id uint32]...
+        ByteBuffer buffer = ByteBuffer.allocate(65536);
+        
+        // Marker byte to indicate this is a channel map
+        buffer.put((byte) 0xFF);
+        
+        // Number of channels
+        buffer.putShort((short) channelMap.size());
+        
+        // For each channel, write: uint16 name_length, string name, uint32 channel_id
+        for (Map.Entry<String, Integer> entry : channelMap.entrySet()) {
+            String name = entry.getKey();
+            int id = entry.getValue();
+            byte[] nameBytes = name.getBytes(StandardCharsets.UTF_8);
+            
+            buffer.putShort((short) nameBytes.length);
+            buffer.put(nameBytes);
+            buffer.putInt(id);
+        }
+        
+        // Write the buffer
+        int length = buffer.position();
+        currentClientOut.write(buffer.array(), 0, length);
+        currentClientOut.flush();
+        
+        System.out.println("[wts] Sent channel map with " + channelMap.size() + " channels");
+    }
+
     public void sendData(String key, double value) {
-//        if (this.currentClient != null && this.currentClient.isConnected() && !this.currentClient.isOutputShutdown()) {
-            this.messageQueue.add(("event: data\r\n" + String.format("data: {\"%s\": %f}\r\n", key, value) + "\r\n"));
-//        }
+        // Get or assign channel ID
+        int channelId = channelMap.computeIfAbsent(key, k -> nextChannelId.getAndIncrement());
+        
+        // If this is a new channel and a client is connected, we should send updated map
+        // For now, we'll just send the data with the channel ID
+        
+        // Binary format: uint16 length, int32 channel_id, double value
+        ByteBuffer buffer = ByteBuffer.allocate(2 + 4 + 8);
+        buffer.putShort((short) (4 + 8)); // length of (channel_id + value)
+        buffer.putInt(channelId);
+        buffer.putDouble(value);
+        
+        this.messageQueue.add(buffer.array());
     }
 
 }
